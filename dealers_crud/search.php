41314edd57051177ec25528ed786212039496d7b
Nicole_S <shibiyun2013@163.com>
2017-04-26 10:46:40 -0500

    Update search.php

--- dealers_crud/search.php
+++ dealers_crud/search.php
@@ -1,22 +1,61 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Book Search</title>
+        <title>Car Search</title>
+        <script src="/scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
         <style type="text/css">
-            table{
-                background-color: #663399;
-                color:yellow;
+            .t1
+            {
+                clear: both;
+                border: 1px solid #c9dae4;
             }
-            th{
-                border-bottom:150px;
-                text-align: left;
+            .t1 tr th
+            {
+                color: #0d487b;
+                background: #f2f4f8 repeat-x left bottom;
+                line-height: 28px;
+                border-bottom: 1px solid #9cb6cf;
+                border-top: 1px solid #e9edf3;
+                font-weight: normal;
+                text-shadow: #e6ecf3 1px 1px 0px;
+                padding-left: 5px;
+                padding-right: 5px;
+            }
+            .t1 tr td
+            {
+                border-bottom: 1px solid #e9e9e9;
+                padding-bottom: 5px;
+                padding-top: 5px;
+                color: #444;
+                border-top: 1px solid #FFFFFF;
+                padding-left: 5px;
+                padding-right: 5px;
+                word-break: break-all;
+            }
+            tr.alt td
+            {
+                background: #ecf6fc;
+            }
+            tr.over td
+            {
+                background: #bcd4ec;
             }
         </style>
+        <script type="text/javascript">
+            $(document).ready(function () {  
+                $(".t1 tr").mouseover(function () { 
+                    $(this).addClass("over");
+                }).mouseout(function () { 
+                    $(this).removeClass("over");
+                })
+                $(".t1 tr:even").addClass("alt");
+            });
+        </script>
     </head>
     <body>
-    <hl>Dealer search</hl>
+    <h1 style = "color: gray; font-family: Courier New;">Search for car: </h1>
     
-    <form method="post" action ="search.php">
+    <form method="post" action ="search.php" align="center" style = "font-weight: bold">
         <input type="hidden" name="submitted" value="true" />
         
         <label>Search Category:
@@ -48,12 +87,12 @@
         $query = "SELECT * FROM Cars WHERE $category = '$criteria'";
         $result = mysqli_query($mysqli, $query) or die('error getting data');
         
-        echo "<table>";
+        echo "<table width='80%' border='0' class='t1' align='center'>";
         echo "<tr><th>Vin</th><th>Model</th><th>Make</th><th>Year</th><th>Mileage</th><th>Bno</th><th>Rrp</th></tr>";
         
         while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
             
-            echo "<tr><td>";
+            echo "<tr align='center'><td>";
             echo $row['Vin'];
             echo "</td><td>";
             echo $row['Model'];
@@ -76,11 +115,13 @@
     }// end of main if statement   
     
     ?>
-    <form action="index.php">
-		<input type="submit" value="Back to Home">
-
+    <form action="index.html">
+        <br><br><br>
+        <div align="center">
+            <input type="submit" value="Back to Home">
+        </div>
 	</form>
-    
+	    
     </body>
 </html>
 
